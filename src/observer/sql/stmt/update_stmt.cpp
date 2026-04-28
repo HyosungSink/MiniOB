88643f11b23417fb6720defc5087b5cf03b0001a
HyosungSink <1119397793@qq.com>
2026-04-28 10:09:31 +0800

    Bind update statements

--- src/observer/sql/stmt/update_stmt.cpp
+++ src/observer/sql/stmt/update_stmt.cpp
@@ -13,14 +13,69 @@ See the Mulan PSL v2 for more details. */
 //
 
 #include "sql/stmt/update_stmt.h"
+#include "common/lang/string.h"
+#include "common/log/log.h"
+#include "sql/stmt/filter_stmt.h"
+#include "storage/db/db.h"
+#include "storage/table/table.h"
 
-UpdateStmt::UpdateStmt(Table *table, Value *values, int value_amount)
-    : table_(table), values_(values), value_amount_(value_amount)
+UpdateStmt::UpdateStmt(Table *table, const FieldMeta *field_meta, const Value &value, FilterStmt *filter_stmt)
+    : table_(table), field_meta_(field_meta), value_(value), filter_stmt_(filter_stmt)
 {}
+
+UpdateStmt::~UpdateStmt()
+{
+  if (filter_stmt_ != nullptr) {
+    delete filter_stmt_;
+    filter_stmt_ = nullptr;
+  }
+}
 
 RC UpdateStmt::create(Db *db, const UpdateSqlNode &update, Stmt *&stmt)
 {
-  // TODO
   stmt = nullptr;
-  return RC::INTERNAL;
+
+  const char *table_name = update.relation_name.c_str();
+  if (db == nullptr || common::is_blank(table_name) || common::is_blank(update.attribute_name.c_str())) {
+    LOG_WARN("invalid update statement. db=%p, table=%s, field=%s", db, table_name, update.attribute_name.c_str());
+    return RC::INVALID_ARGUMENT;
+  }
+
+  Table *table = db->find_table(table_name);
+  if (table == nullptr) {
+    LOG_WARN("no such table. db=%s, table=%s", db->name(), table_name);
+    return RC::SCHEMA_TABLE_NOT_EXIST;
+  }
+
+  const FieldMeta *field_meta = table->table_meta().field(update.attribute_name.c_str());
+  if (field_meta == nullptr) {
+    LOG_WARN("no such field. table=%s, field=%s", table_name, update.attribute_name.c_str());
+    return RC::SCHEMA_FIELD_NOT_EXIST;
+  }
+
+  Value value;
+  RC rc = RC::SUCCESS;
+  if (update.value.attr_type() == field_meta->type()) {
+    value = update.value;
+  } else {
+    rc = Value::cast_to(update.value, field_meta->type(), value);
+    if (OB_FAIL(rc)) {
+      LOG_WARN("failed to cast update value. table=%s, field=%s, rc=%s", table_name, field_meta->name(), strrc(rc));
+      return rc;
+    }
+  }
+
+  unordered_map<string, Table *> table_map;
+  table_map.insert(pair<string, Table *>(string(table_name), table));
+
+  FilterStmt *filter_stmt = nullptr;
+  rc = FilterStmt::create(
+      db, table, &table_map, update.conditions.data(), static_cast<int>(update.conditions.size()), filter_stmt);
+  if (OB_FAIL(rc)) {
+    LOG_WARN("failed to create update filter. rc=%s", strrc(rc));
+    return rc;
+  }
+
+  stmt = new UpdateStmt(table, field_meta, value, filter_stmt);
+  return RC::SUCCESS;
 }
