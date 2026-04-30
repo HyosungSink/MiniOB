1e91b0039e3594ced24d4c6de471b7cc68fc9eb4
HyosungSink <1119397793@qq.com>
2026-04-30 11:00:23 +0000

    Wire date type conversions

--- src/observer/common/type/char_type.cpp
+++ src/observer/common/type/char_type.cpp
@@ -11,6 +11,7 @@ See the Mulan PSL v2 for more details. */
 #include "common/lang/comparator.h"
 #include "common/log/log.h"
 #include "common/type/char_type.h"
+#include "common/type/date_type.h"
 #include "common/value.h"
 
 int CharType::compare(const Value &left, const Value &right) const
@@ -29,6 +30,15 @@ RC CharType::set_value_from_str(Value &val, const string &data) const
 RC CharType::cast_to(const Value &val, AttrType type, Value &result) const
 {
   switch (type) {
+    case AttrType::DATES: {
+      int date = 0;
+      RC rc = DateType::parse_date(val.get_string(), date);
+      if (OB_FAIL(rc)) {
+        return rc;
+      }
+      result.set_date(date);
+      return RC::SUCCESS;
+    }
     default: return RC::UNIMPLEMENTED;
   }
   return RC::SUCCESS;
@@ -38,6 +48,8 @@ int CharType::cast_cost(AttrType type)
 {
   if (type == AttrType::CHARS) {
     return 0;
+  } else if (type == AttrType::DATES) {
+    return 1;
   }
   return INT32_MAX;
 }
